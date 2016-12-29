9c1e6b770d9e135a5a66a91ba2d101eb3e1153a3
sp0dy <592504835@qq.com>
2016-12-29 16:43:14 +0800

    二次提交

---
+++ src/main/java/com/example/administrator/my_ui/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.administrator.my_ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Button button = (Button) findViewById(R.id.button_ui);
+//        final EditText editText = (EditText) findViewById(R.id.et_ui);
+
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String str = editText.getText().toString();
+//                Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+}
